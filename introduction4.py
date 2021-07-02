b506dec30aae6229494aa86446f329b76036cf35
shunta <s.goto@kaoiri.co.jp>
2021-07-02 23:08:41 +0900

    finished introduction4

---
+++ introduction4.py
@@ -0,0 +1,47 @@
+# print(1 + 3)
+# print(1 - 3)
+# print(1 * 3)
+# print(1 / 3)
+
+# a = 1
+# b = 3
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+
+# tasu = 1 + 3
+# hiku = 1 - 3
+# kakeru = 1 * 3
+# waru = 1 / 3
+# print(tasu)
+# print(hiku)
+# print(kakeru)
+# print(waru)
+
+# print(1 // 3)
+# print(1 % 3)
+
+# print("これは文字です")
+# print('これは文字です')
+
+# s = 'これは文字です'
+# print(s)
+# print(s[0])
+# print(s[0:3])
+# print(s[:3])
+# print(s[3:])
+# print(s[-1])
+
+# a = 2
+# print(a == 2)
+# print(a == 3)
+# print(a >= 3)
+# print(a >= 2)
+# print(a > 2)
+# print(a < 2)
+# print(a <= 2)
+
+# is_ok = True
+# is_ok == True
+# is_ok == False
