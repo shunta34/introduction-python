2dd897e6302c2170675c8c1d49ae931bdd79f16d
shunta <s.goto@kaoiri.co.jp>
2021-07-04 23:46:01 +0900

    finished introduction11

---
+++ introduction11.py
@@ -0,0 +1,16 @@
+# l = []
+# for i in range(5):
+#   l.append(i)
+# print(l)
+
+# l = [i for i in range(5)]
+# print(l)
+
+# l = [i for i in range(5) if i % 2 == 0]
+# print(l)
+
+# l = []
+# for i in range(5):
+#   if i % 2 == 0:
+#     l.append(i)
+# print(l)
