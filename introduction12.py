ca4955e4d34cf4c766e4bbb412e5d056a226e438
shunta <s.goto@kaoiri.co.jp>
2021-07-06 00:39:06 +0900

    finished introduction12

---
+++ introduction12.py
@@ -0,0 +1,22 @@
+# t = (1, 2, 3)
+# print(t)
+# print(type(t))
+# t[0] = 10
+# print(t[0])
+
+# t = (1, 2)
+# one, two = t
+# print('1', one)
+# print('2', two)
+# print(t[0])
+# print(t[1])
+
+# t1 = (1, 2)
+# t2 = (3, 4)
+# print(t1)
+# print(t2)
+# print('t1: ',id(t1))
+# print('t2: ',id(t2))
+# t1 = t1 + t2
+# print(t1)
+# print(id(t1))
