25cc7b6462676c900466f7b0bfb5569428cbcdf2
shunta <s.goto@kaoiri.co.jp>
2021-07-07 00:21:33 +0900

    finished introduction13

---
+++ introduction13.py
@@ -0,0 +1,39 @@
+# d = {'apple': 80, 'banana': 100}
+# print(d)
+# print(type(d))
+# print(d['apple'])
+# print(d['banana'])
+# d['banana'] = 120
+# print(d)
+# d['peach'] = 150
+# print(d)
+
+# d = {'apple': 80, 'banana': 100, 'peach': 150}
+# print(d)
+# print(d.keys())
+# for key in d.keys():
+#   print(key)
+# print(d.values())
+# for value in d.values():
+#   print(value)
+# print(d.items())
+# for key, value in d.items():
+#   print(key, value)
+
+# t = (1, 2)
+# one, two = t
+# print(one, two,)
+
+# d = {'apple': 80, 'banana': 100, 'peach': 150}
+# print(d['apple'])
+# print(d)
+# print(d.get('apple'))
+# print(d.get('meron'))
+# print(d['meron'])
+
+# d = {'apple': 80, 'banana': 100, 'peach': 150}
+# print(d)
+# print(d['apple'])
+# print(d)
+# print(d.pop('apple'))
+# print(d)
