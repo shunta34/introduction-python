0fc32dd30c5b8f63ca772a6bc64f2e6929e631ea
shunta <s.goto@kaoiri.co.jp>
2021-07-07 22:04:33 +0900

    finished introduction15

---
+++ introduction14.py
@@ -0,0 +1,21 @@
+# a = {1, 2, 3, 4, 5}
+# print(a)
+# print(type(a))
+
+# b = {2, 3, 3, 4, 5, 7, 9}
+# print(b)
+# print(type(b))
+
+# c = {1, 4, 3, 2}
+# print(c)
+# print(type(c))
+
+# a = {1, 2, 3, 4, 5}
+# print('a', a)
+# b = {2, 3, 5, 7, 9}
+# print('b', b)
+# print(a - b)
+# print(b - a)
+# print(a & b)
+# print(a | b)
+# print(a ^ b)
---
+++ introduction15.py
@@ -0,0 +1,47 @@
+# l = [1, 2, 3]
+# print(max(l))
+# print(min(l))
+
+# def say_hello():
+#   print('Hello!!!')
+# say_hello()
+# say_hello()
+# say_hello()
+
+# def say_hello():
+#   s = 'Hello!!!'
+#   return s
+# returned_s = say_hello()
+# print(returned_s)
+
+# def fizzbuzz():
+#   x = 3
+#   if x % 15 == 0:
+#     result = 'FizzBuzz'
+#   elif x % 3 == 0:
+#     result = 'Fizz'
+#   elif x % 5 == 0:
+#     result = 'Buzz'
+#   else:
+#     result = x
+#   return result
+# r = fizzbuzz()
+# print(r)
+
+# def fizzbuzz(x):
+#   if x % 15 == 0:
+#     result = 'FizzBuzz'
+#   elif x % 3 == 0:
+#     result = 'Fizz'
+#   elif x % 5 == 0:
+#     result = 'Buzz'
+#   else:
+#     result = x
+#   return result
+# r = fizzbuzz(3)
+# print(r)
+# r1 = fizzbuzz(3)
+# r2 = fizzbuzz(5)
+# r3 = fizzbuzz(30)
+# r4 = fizzbuzz(49)
+# print(r1, r2, r3, r4)
