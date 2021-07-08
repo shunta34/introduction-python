4a08d33dd7bc8e8bf0bd9d7ffecb8c32b3bc8942
shunta <s.goto@kaoiri.co.jp>
2021-07-08 23:04:45 +0900

    finished introduction17

---
+++ introduction16.py
@@ -0,0 +1,35 @@
+# def f(x):
+#   return x + 3
+# print(f(2))
+
+# def get_full_name(first, name):
+#   return first + ' ' + name
+# r = get_full_name('佐藤', '太郎')
+# print(r)
+# r = get_full_name('太郎', '佐藤')
+# print(r)
+# r = get_full_name(name = '太郎', first = '佐藤')
+# print(r)
+
+# def half_value(x):
+#   return x / 2
+# r = half_value(2)
+# print(r)
+
+# half_value_v2 = lambda x: x / 2
+# r2 = half_value_v2(2)
+# print(r2)
+
+# def f(x):
+#   if x % 2 == 0:
+#     return x
+#   else:
+#     return '2で割り切れません!!'
+# r = f(2)
+# print(r)
+
+# f2 = lambda x: x if x % 2 == 0 else '2で割り切れません!!'
+# r2 = f2(2)
+# print(r2)
+# r2 = f2(3)
+# print(r2)
---
+++ introduction17.py
@@ -0,0 +1,73 @@
+# add_num = lambda a, b: a + b
+# add_num(1, '2')
+
+# try:
+#   add_num = lambda a, b: a + b
+#   r = add_num(1, '2')
+# except:
+#   print('例外が発生しました')
+# print('例外処理を終わりました。')
+
+# add_num = lambda a, b: a + b
+# try:
+#   r = add_num(1, '2')
+#   print(r)
+# except TypeError as e:
+#   print(e)
+#   print('例外が発生しました')
+# print('例外処理を終わりました。')
+
+# add_num = lambda a, b: a + b
+# try:
+#   r = add_num(1, 2)
+#   print(r)
+# except TypeError as e:
+#   print(e)
+#   print('例外が発生しました')
+# print('例外処理を終わりました。')
+
+# add_num = lambda a, b: a + b
+# try:
+#   r = add_num(1, 2)
+# except TypeError as e:
+#   print(e)
+#   print('例外が発生しました')
+# else:
+#   print(r)
+# print('例外処理を終わりました。')
+
+# add_num = lambda a, b: a + b
+# try:
+#   r = add_num(1, '2')
+# except TypeError as e:
+#   print(e)
+#   print('例外が発生しました')
+# else:
+#   print(r)
+# print('例外処理を終わりました。')
+
+# add_num = lambda a, b: a + b
+# try:
+#   r = add_num(1, 2)
+# except TypeError as e:
+#   print(e)
+#   print('例外が発生しました')
+# else:
+#   print(r)
+# finally:
+#   print('例外処理を終わりました!')
+# print('**********')
+# print('例外処理を終わりました。')
+
+# add_num = lambda a, b: a + b
+# try:
+#   r = add_num(1, '2')
+# except TypeError as e:
+#   print(e)
+#   print('例外が発生しました')
+# else:
+#   print(r)
+# finally:
+#   print('例外処理を終わりました!')
+# print('**********')
+# print('例外処理を終わりました。')
