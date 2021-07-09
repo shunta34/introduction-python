48c4d0beddb9d612e25bc20c6a1a904a6a950172
shunta <s.goto@kaoiri.co.jp>
2021-07-09 23:40:57 +0900

    finished introduction19

---
+++ introduction18.py
@@ -0,0 +1,15 @@
+# l = [1, 2, 3]
+# l.append(4)
+# print(l)
+# help(l)
+
+# class Child(object):
+#   def say_hello(self):
+#     print('year!!!')
+# child = Child()
+# child.say_hello()
+
+# l = list()
+# print(l)
+# l.append(1)
+# print(l)
---
+++ introduction19.py
@@ -0,0 +1,38 @@
+# class Child(object):
+#   def __init__(self):
+#     print('Waa!!!')
+#   def say_hello(self):
+#     print('year!!!')
+# child = Child()
+
+# class Child(object):
+#   def __init__(self, name):
+#     self.name = name
+#     print('My name is ', self.name)
+#   def say_hello(self):
+#     print('year!!!')
+# child = Child('Taro')
+
+# class Child(object):
+#   def __init__(self, name):
+#     self.name = name
+#     print('My name is ', self.name)
+#   def say_hello(self):
+#     print('year!!!' , self.name)
+# child = Child('Taro')
+# child.say_hello()
+
+# class Child(object):
+#   def __init__(self, name):
+#     self.name = name
+#     print('My name is ', self.name)
+#   def say_hello(self):
+#     print('year!!!' , name)
+# child = Child('Taro')
+# child.say_hello()
+
+# class Child(object):
+#   def __init__(self):
+#     print(self)
+# child = Child()
+# print(child)
